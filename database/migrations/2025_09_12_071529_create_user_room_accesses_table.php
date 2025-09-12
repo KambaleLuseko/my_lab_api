ea8f34d01ac6ff01ff8138cf3d32f61577c13452
KambaleLuseko <providencekambaleluseko@gmail.com>
2025-09-12 10:32:32 +0200

    user room access controller integration

---
+++ database/migrations/2025_09_12_071529_create_user_room_accesses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_room_accesses', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->string('user_uuid');
+            $table->string('room_uuid');
+            $table->date('date');
+            $table->string('start_time');
+            $table->string('end_time');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_room_accesses');
+    }
+};
